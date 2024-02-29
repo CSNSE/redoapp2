fb38ecdc9fc5fba57eaa6249d7b07a840680199b
Clement-Wright <clewri163@gmail.com>
2024-02-29 11:26:35 -0500

    https://github.com/CSNSE/redoapp2/issues/26#issue-2127283154

--- src/App.js
+++ src/App.js
@@ -8,35 +8,13 @@ import EditNote from './EditNote';
 import DesmosCalculator from './DesmosCalculator';
 
 class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            redirectToHomePage: true // Set to true initially to redirect on startup
-            
-        };
-    }
-
-    componentDidMount() {
-        // After 3 seconds, stop redirecting to HomePage.html
-        setTimeout(() => {
-            this.setState({ redirectToHomePage: false });
-        }, 1000); // Adjust the delay as needed
-    }
-
     render() {
-        const { redirectToHomePage } = this.state;
 
-        // If redirectToHomePage is true, redirect to HomePage.html
-        if (redirectToHomePage) {
-            return null; // You can render a loading spinner or message here if needed
-        }
-
-        // Once redirectToHomePage is false, render the app with the routes
         return (
             <div className="App">
                 <header className="App-header">
                     <Routes>
-                        <Route path="/" element={<HomePage/>} />
+                        <Route path="/" element={<EquationNavBar/>} />
                         <Route exact path='/History' element={<div><EquationNavBar/><EquationImageNoteCardCollection isPaginated itemsPerPage={2} /></div>} />
                         <Route exact path='/NewNote' element={<NewNote/>}/>
                         <Route exact path='/update/:cid' element={<EditNote/>}/>
@@ -46,11 +24,5 @@ class App extends Component {
         );
     }
 }
-//This is an empty commite because I actually 
-// Placeholder component for HomePage.html if needed
-const HomePage = () => {
-    // You can adjust this to serve HomePage.html or redirect to it if it's a route
-    window.location.href = "/HomePage.html";
 
-};
 export default withAuthenticator(App);
